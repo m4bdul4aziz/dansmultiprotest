6fe5b8c13901d278013e071602bf946e7d950b47
m4bdul4aziz <m4bdul4aziz@gmail.com>
2023-01-28 06:44:42 +0700

    Update database.js
    add comment

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ let mysql = require('mysql');
 
 const dotenv = require("dotenv");
 dotenv.config();
-// Mongo DB conncetion
+// mysql DB conncetion
 const db_host = process.env.DB_HOST;
 const db_users = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
